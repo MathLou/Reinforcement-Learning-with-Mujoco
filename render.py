ac5162830ceef6655dbc59171ea69548a9491b76
MathMaker <35754208+MathLou@users.noreply.github.com>
2025-06-26 01:27:59 -0300

    Update render.py

--- render.py
+++ render.py
@@ -1,7 +1,7 @@
 from stable_baselines3 import PPO
-import gym
+import gymnasium as gym
 import numpy as np
-from gym import spaces
+from gymnasium import spaces
 import mujoco
 from mujoco import MjModel, MjData, viewer
 import time
@@ -59,11 +59,12 @@ class FurutaEnv(gym.Env):
 
 
 env = FurutaEnv(render=True)
-model = PPO.load("ppo_furuta")
+model = PPO.load("ppo_furuta_10M_PPO13_2M", env=env)
 
 obs, _ = env.reset()
 while True:
     action, _ = model.predict(obs)
     obs, reward, done, _, _ = env.step(action)
     env.render()
-    time.sleep(0.004)
+    print(f"Reward: {reward}, Observation (pendulum angle, pendulum angular vel.,arm position, arm ang. vel.): {obs}")
+    time.sleep(0.03)
